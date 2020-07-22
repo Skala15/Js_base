7143c5f3cf27f5d3d7ff2d5d156c96a309ba4548
skala_15 <idanilas@mail.ru>
2020-07-22 22:32:14 +0300

    задание после третьего урока на проверку

---
+++ lesson_3/script/main.js
@@ -0,0 +1,55 @@
+//Задание №1 
+/*
+let n = 0
+while (n <= 100) {
+	console.log(n)
+	n++
+}
+*/
+
+
+
+
+
+
+//Задание №2-3
+/*
+let arr = [3300, 1100, 2200, 2500]
+
+function countBasketPrice(arr) {
+	let sum = 0
+	for (let i = 0; i < arr.length; i++) {
+		sum += arr[i]
+	}
+	console.log(sum)
+}
+
+countBasketPrice(arr)
+*/
+
+
+
+
+
+
+//Задание №4 
+/*
+for (let n = 0; n < 10; console.log(n++)) {
+}
+*/
+
+
+
+
+
+
+//Задание №4* 
+/*
+	let a ="*"
+	let b = a
+
+	while (b.length <= 20) {
+		console.log(b)
+		b = b + a
+	}
+*/
