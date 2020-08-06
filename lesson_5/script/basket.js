f32e070bc48a716000738d86937fe917231af16d
skala_15 <idanilas@mail.ru>
2020-08-06 10:47:25 +0300

    Сделано 5 задание

--- lesson_5/script/basket.js
+++ lesson_5/script/basket.js
@@ -1,3 +1,5 @@
+// Задание 2
+
 let basket_price = 0
 
 const object = {
@@ -16,4 +18,12 @@ const object = {
 	}
 }
 
-object.countBasketPrice()
+
+document.write("<h2>В корзина:</h2>")
+
+for (i=0; i < object.arr.length; i++) {
+	document.write(object.arr[i].product + ' — ' + object.arr[i].price + " руб</br>")
+}
+
+document.write("</br><b>В корзине: " + "</b>" + object.arr.length + " товара")
+document.write("</br><b>Стоимость заказа: "  + "</b>" + object.countBasketPrice())
--- lesson_5/script/main.js
+++ lesson_5/script/main.js
@@ -29,30 +29,3 @@ function myBoard(){
 }
  
 myBoard()
-
-Window.onload = my_initiation
-
-
-
-
-
-//Задание 2
-let basket_price = 0
-
-const object = {
-
-	arr: [{product: "shorts", price: 2200}, {product: "cap", price: 1100}, {product: "shoes", price: 3300}, {product: "socks", price: 220}],
-	
-	countBasketPrice: function () {
-		
-		for(i = 0; i < this.arr.length; i++) {
-			basket_price = parseInt(basket_price + this.arr[i].price)
-
-		}
-
-		console.log(basket_price)
-		return 
-	}
-}
-
-object.countBasketPrice()
